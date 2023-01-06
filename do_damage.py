45fee8dd344245b040c7fe93e84d4f069d56be15
Alessio Di Giacomo <aledigiacom@gmail.com>
2023-01-06 13:08:32 +0100

    Aggiorna script con image_generator e con dati freschi

---
+++ do_damage.py
@@ -0,0 +1,116 @@
+import requests
+import sqlite3
+import pprint
+import os
+import random
+from instagrapi import Client
+from datetime import datetime, timezone
+from image_generator.generate_image import generate_image
+
+DRY_RUN_ENABLED=True
+
+PATH_FOLDER_BASE_IMAGES = os.path.abspath("folder_base_images")
+PATH_FOLDER_GENERATED_IMAGES = os.path.abspath("folder_generated_images")
+
+COMFUCIUS_GET_PHRASE_PUBLIC_API_URL = 'https://comfucius.xyz/quotes/api/get-fake-quote'
+
+INSTAGRAM_USERNAME=os.environ.get('INSTAGRAM_USERNAME')
+INSTAGRAM_PASSWORD=os.environ.get('INSTAGRAM_PASSWORD')
+
+INSTAGRAM_POST_GOOD_WORD_LIMIT=20
+INSTAGRAM_POST_BAD_WORD_LIMIT=1
+
+def get_instagram_post_caption(good_word_limit, bad_word_limit):
+    '''
+    Get instagram caption concatenating a series of "good word" hashtags (e.g. philosophy, love)
+    and a series of "bad" ones (e.g. atomicbomb, backache)
+    '''
+
+    sqlite_db = sqlite3.connect("database.sqlite")
+
+    hashtags_good = []
+    hashtags_bad = []
+
+    output_obj = sqlite_db.execute("SELECT word FROM hashtag WHERE type == 'good' ORDER BY RANDOM() LIMIT "+str(good_word_limit))
+
+    for single_row in output_obj.fetchall():
+        hashtags_good.append('#'+single_row[0])
+
+    output_obj = sqlite_db.execute("SELECT word FROM hashtag WHERE type == 'bad' ORDER BY RANDOM() LIMIT "+str(bad_word_limit))
+
+    for single_row in output_obj.fetchall():
+        hashtags_good.append('#'+single_row[0])
+
+    return (f'{" ".join(hashtags_good)} {" ".join(hashtags_bad)}')
+
+def post_photo_on_instagram(image_path, post_caption, accessibility_caption):
+    '''
+    Post a .jpg image on instagram as a photo post. Use data generated early
+    Instaclient doc URL: https://adw0rd.github.io/instagrapi/usage-guide/media.html
+
+    If "dry run", don't post anything on insta, just print info locally
+    '''
+
+    if DRY_RUN_ENABLED is True:
+
+        # Avoid actual posting on Instagram
+        pprint.pprint(locals())
+
+    else:
+
+        instaclient = Client()
+        instaclient.login(INSTAGRAM_USERNAME, INSTAGRAM_PASSWORD)
+
+        media = instaclient.photo_upload(
+            image_path,
+            post_caption,
+            extra_data={
+                "custom_accessibility_caption": accessibility_caption,
+            }
+        )
+
+        pprint.pprint(media.dict())
+
+if __name__ == "__main__":
+
+    # Get phrase from Comfucius
+
+    comfucius_phrase_data = requests.get(COMFUCIUS_GET_PHRASE_PUBLIC_API_URL).json()
+    comfucius_complete_quote = f'{comfucius_phrase_data["Phrase"]} ~ {comfucius_phrase_data["Thinker"]}'
+    
+    # Generate a .jpg photo, using a random base image
+    
+    background_image_filename=random.choice(os.listdir(PATH_FOLDER_BASE_IMAGES))
+    background_image_filepath=PATH_FOLDER_BASE_IMAGES + os.sep + background_image_filename
+
+    generated_image_filename_no_extension = f'generated_image___{datetime.utcnow().strftime("%Y-%m-%d_%I-%M-%S_%p")}'
+
+    generated_image_path = generate_image(
+        background_image_filepath, 
+        PATH_FOLDER_GENERATED_IMAGES, 
+        generated_image_filename_no_extension,
+        comfucius_complete_quote
+    )
+
+    # Generate a cool caption
+
+    generated_post_caption = get_instagram_post_caption(
+        good_word_limit=INSTAGRAM_POST_GOOD_WORD_LIMIT, 
+        bad_word_limit=INSTAGRAM_POST_BAD_WORD_LIMIT
+    )
+
+    # Post on Instagram, if "DRY_RUN_ENABLED" is True
+
+    post_result_info = post_photo_on_instagram(
+        image_path="generated-image.jpg", # TODO: use real variable
+        post_caption=generated_post_caption,
+        accessibility_caption=comfucius_complete_quote
+    )
+
+    print(f"Post done! Date: {datetime.now()}")
+
+    # Delete the used .jpg photo, if "DRY_RUN_ENABLED" is True
+
+    # TODO:
+
+
---
+++ image_generator/generate_image.py
@@ -0,0 +1,71 @@
+# coding=utf-8
+import textwrap
+import os
+from PIL import Image, ImageFont, ImageDraw, ImageOps
+
+def generate_image(path_background_image, generated_image_directory, generated_image_filename_no_extension, text_to_use):
+
+    # Carica l'immagine di sfondo
+    background_image = Image.open(path_background_image)
+
+    # Scala fino alla risoluzione ideale
+    background_image = ImageOps.fit(background_image, (1080, 1350), centering=(0.5,0.5))
+
+    # Converti l'immagine di sfondo in formato RGBA
+    background_image = background_image.convert("RGBA")
+
+    # Crea un'immagine di dimensioni pari all'immagine di sfondo con un colore scuro
+    overlay = Image.new("RGBA", background_image.size, (0, 0, 0, 150))
+
+    # Sovrapponi l'immagine di sfondo con l'overlay scuro
+    background_image = Image.alpha_composite(background_image, overlay)
+
+    # Crea un oggetto draw per disegnare sull'immagine
+    draw = ImageDraw.Draw(background_image)
+
+    # Converti tutto in maiuscolo, e divide la frase motivazionale in due parti
+    phrase, author = text_to_use.upper().split(" ~ ")
+
+    ## Calculate font size
+
+    draw = ImageDraw.Draw(background_image)
+    fontsize = 1  # starting font size
+
+    # La frase è troppo lunga? Dividiamola in parti accettabili
+    phrase_lines = textwrap.wrap(phrase, width=24)
+    longest_phrase_line = max(phrase_lines, key=len);
+
+    # portion of background_image width you want text width to be
+    img_fraction = 0.80
+
+    font_path_for_phrase = os.path.abspath("image_generator" + os.sep + "fonts") + os.sep + "MerriweatherSans-SemiBold.ttf"
+    font_path_for_author = os.path.abspath("image_generator" + os.sep + "fonts") + os.sep + "Caveat-Medium.ttf"
+
+    font = ImageFont.truetype(font_path_for_phrase, fontsize)
+    breakpoint = img_fraction * background_image.size[0]
+    jumpsize = 75
+
+    # Calcola la dimensione del font basandosi solo sulla frase
+    while True:
+        if font.getlength(longest_phrase_line) < breakpoint:
+            fontsize += jumpsize
+        else:
+            jumpsize = jumpsize // 2
+            fontsize -= jumpsize
+        font = ImageFont.truetype(font_path_for_phrase, fontsize)
+        if jumpsize <= 1:
+            break
+
+    # optionally de-increment to be sure it is less than criteria
+    fontsize -= 1
+
+    # Disegna la frase
+    phrase_coordinates = draw.multiline_text((background_image.width/2,background_image.height/2), "\n".join(phrase_lines), spacing=40, align="center", anchor="mm", font=font, fill=(255, 255, 255))
+
+    (left, top, right, bottom) = draw.multiline_textbbox((background_image.width/2,background_image.height/2), "\n".join(phrase_lines), spacing=40, align="center", anchor="mm", font=font)
+
+    # Calcola le coordinate dell'autore in modo da posizionarlo sotto la frase
+    draw.text((background_image.width/2, bottom+40),"\n\n~ "+author, align="center", anchor="mm", font=ImageFont.truetype(font_path_for_author, int(fontsize/2)), fill=(255, 255, 255))
+
+    # Salva l'immagine
+    background_image.convert('RGB').save(generated_image_directory + os.sep + generated_image_filename_no_extension +".jpg")
